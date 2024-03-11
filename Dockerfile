8c444f8c62885a5f1a3273a8ceb560b8d4e4162e
terrencegabrielvida <68225312+terrencegabrielvida@users.noreply.github.com>
2024-03-11 13:48:50 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:alpine
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN yarn
+
+COPY . .
+
+RUN yarn build
+
+EXPOSE 3000
+CMD [ "yarn", "start:prod" ]
---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getCICD(): string {
+    return 'This is Terrence CICD Practice!';
+  }
+}
